3e08e9b712d52351703c4cf2c003876fd2f821c1
No1zee <edwitter123magejo@gmail.com>
2026-08-07 13:10:23 +0200

    fix(config): clean next.config.ts remotePatterns declaration for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    localPatterns: [
-      {
-        pathname: "/**",
-      },
-    ],
     remotePatterns: [
       {
         protocol: "https",
